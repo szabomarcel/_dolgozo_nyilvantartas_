ddfc10576bed41e8e62e3f4d36b9bb204d255b3a
szabomarcel <szabomarcell34@gmail.com>
2024-03-11 17:39:57 +0100

    feladat

--- _dolgozo_nyilvantartas_console/Program.cs
+++ _dolgozo_nyilvantartas_console/Program.cs
@@ -15,11 +15,14 @@ namespace _dolgozo_nyilvantartas_console
         static async Task Main(string[] args)
         {
             List<Nyilvantartas> nyilvantartas = new List<Nyilvantartas>();
+            //List<Nyilvantartas> nyilvantartas2 = new List<Nyilvantartas>();
             nyilvantartas = await dolgozoNyilvantartasconsole();
-            foreach (Nyilvantartas nyilvantart in nyilvantartas)
+            Console.WriteLine($"Dolgozok száma: {nyilvantartas.Count()}");
+            /*foreach (Nyilvantartas nyilvantart in nyilvantartas)
             {
-                Console.WriteLine($"{nyilvantart.Id} - {nyilvantart.Salary} - {nyilvantart.Name}");
-            }
+                Console.WriteLine($"{nyilvantart.Salary} - {nyilvantart.Name}");
+            }*/
+            //nyilvantartas2 = await munkakor();
             await Console.Out.WriteLineAsync("Program vége");
             Console.ReadLine();
         }
@@ -34,8 +37,20 @@ namespace _dolgozo_nyilvantartas_console
             {
                 string jsonString = await response.Content.ReadAsStringAsync();
                 nyilvantartas = Nyilvantartas.FromJson(jsonString).ToList();
-            }            
+            }
             return nyilvantartas;
         }
+
+        /*private static async Task<List<Nyilvantartas>> munkakor()
+        {
+            List<Nyilvantartas> nyilvantartas2 = new List<Nyilvantartas>();
+            var positionCounts = nyilvantartas2.GroupBy(e => e.Position).Select(group => new { Position = group.Key, Count = group.Count() });
+
+            /*foreach (Nyilvantartas nyilvantartas1 in nyilvantartas2)
+            {
+                Console.WriteLine($"{nyilvantartas1.Position}");
+            }
+            return nyilvantartas2;
+        }*/
     }
 }
